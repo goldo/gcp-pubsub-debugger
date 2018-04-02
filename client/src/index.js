51cccb8397a48bebecaf2838558a9beae8bfdbbf
Ysa <isabelle.yim@gmail.com>
2018-04-02 16:38:28 +0200

    Create topic details page + fetch messsages data

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { ApolloProvider } from 'react-apollo';
+import { BrowserRouter } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 import App from './App';
 import { client } from './apolloClient';
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 registerServiceWorker();
