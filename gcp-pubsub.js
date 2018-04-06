32fab7133685397a83c88629ee08dd2598a82696
Franck Achkouyan <achko.franck@gmail.com>
2018-04-06 18:22:05 +0200

    allow api endpoint .env file

--- gcp-pubsub.js
+++ gcp-pubsub.js
@@ -2,6 +2,7 @@ const PubSub = require('@google-cloud/pubsub')
 
 const pubsub = new PubSub({
   projectId: process.env.GCP_PROJECT_ID,
+  apiEndpoint: process.env.GCP_API_ENDPOINT,
   credentials: {
     private_key: process.env.GCP_PRIVATE_KEY,
     client_email: process.env.GCP_CLIENT_EMAIL
